8aaa11aee1119a3211e2f295d548257515567db3
Alyssa Beaton <ehseebe@gmail.com>
2020-07-06 18:56:41 +0000

    Added HTML endpoint

--- express_server.js
+++ express_server.js
@@ -2,19 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 
+//homepage
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+//test urls
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
+//test urls input/output
 app.get('/urls.json', (req,res) => {
   res.json(urlDatabase);
-})
+});
 
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b><body><html>\n');
+});
+
+//server signal
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
