9a85d0116530cff694f7ede10ef85cf692fc77c4
Alyssa Beaton <ehseebe@gmail.com>
2020-07-06 19:18:56 +0000

    Updated with EJS setup

--- express_server.js
+++ express_server.js
@@ -2,27 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 
-//homepage
+//set view engine to ejs
+app.set('view engine', 'ejs');
+
+//index page
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.render('pages/index');
 });
 
-//test urls
-const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com'
-};
-
-//test urls input/output
-app.get('/urls.json', (req,res) => {
-  res.json(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b><body><html>\n');
+//about page
+app.get('/about', (req,res) => {
+  res.render(pages/about);
 });
 
 //server signal
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`${PORT} is the magic port`);
 });
