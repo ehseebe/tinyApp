d8af40c891b69d80821c7901c1dafe696fd9f659
Alyssa Beaton <ehseebe@gmail.com>
2020-07-06 19:58:54 +0000

    Added test content to index to pass to views

--- express_server.js
+++ express_server.js
@@ -7,12 +7,22 @@ app.set('view engine', 'ejs');
 
 //index page
 app.get('/', (req, res) => {
-  res.render('pages/index');
+  const drinks = [
+    { name: 'Bloody Mary', drunkness: 3 },
+    { name: 'Martini', drunkness: 5 },
+    { name: 'Scotch', drunkness: 10 }
+];
+const tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+  res.render('pages/index', {
+    drinks,
+    tagline
+  });
 });
 
 //about page
 app.get('/about', (req,res) => {
-  res.render(pages/about);
+  res.render('pages/about');
 });
 
 //server signal
